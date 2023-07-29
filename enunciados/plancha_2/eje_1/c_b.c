1dabfabca7579c6fa6ad9381f5ba550a198cf86d
Luciano Scola <lucianoluigiscola@gmail.com>
2023-07-28 23:25:52 -0300

    p2 e6

--- enunciados/plancha_2/eje_1/c_b.c
+++ enunciados/plancha_2/eje_1/c_b.c
@@ -1,3 +1,3 @@
 int main(){
-  return (254 + (-1));
+  return -3;
 }
--- enunciados/plancha_2/eje_6/program.s
+++ enunciados/plancha_2/eje_6/program.s
@@ -1,11 +1,66 @@
 .data
 length: .byte 10
-#list: .long 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+list: .long 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
 
+.text
+
+suma:
+      xorq %rax, %rax
+      xorq %rcx, %rcx
+      movq $list, %rsi
+      movb length, %cl 
+
+suma_loop:
+      movl (%rsi), %ebx
+      addl %ebx, %eax
+      addq $4, %rsi
+      loop suma_loop
+      ret
+
+maximo:
+      xorq %rax, %rax
+      xorq %rcx, %rcx
+      movq $list, %rsi
+      movl list, %eax
+      movb length, %cl 
+
+maximo_loop:
+      movl (%rsi), %ebx
+      cmpl %ebx, %eax
+      jge maximo_sigue
+      movl %ebx, %eax
+maximo_sigue:
+      addq $4, %rsi
+      loop maximo_loop
+      ret
+
+minimo:
+      xorq %rax, %rax
+      xorq %rcx, %rcx
+      movq $list, %rsi
+      movl list, %eax
+      movb length, %cl 
+
+minimo_loop:
+      movl (%rsi), %ebx
+      cmpl %ebx, %eax
+      jle minimo_sigue
+      movl %ebx, %eax
+minimo_sigue:
+      addq $4, %rsi
+      loop minimo_loop
+      ret
+
+promedio:
+      call suma
+      movzbl length, %ebx
+      xorl %edx, %edx
+      divl %ebx
+      ret
+      
 
 .global main
 main:
-      xorw %cx, %cx
-      xorq %rax, %rax
-      movl $1234, %ebx
+      #call suma
+      call promedio
       ret
